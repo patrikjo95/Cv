fa7a549b3655df2783c7271d5d8844e3b7c47334
Patik Johansson <patrikjo95@gmail.com>
2021-12-09 23:16:39 +0100

    Lagt till så att knapparna öppnar nya rutor

--- src/main/java/com/example/cv2/Controller.java
+++ src/main/java/com/example/cv2/Controller.java
@@ -2,21 +2,41 @@ package com.example.cv2;
 
 
 import Hangman.Game;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Controller {
 
-    public void aboutButtonClicked(){
-        System.out.println("Om patrik");
+    public void aboutButtonClicked()throws Exception{
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("about.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("About, Patrik J");
+        stage.show();
+    }
+
+    public void careerButtonClicked()throws Exception{
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("career.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("Career, Patrik J");
+        stage.show();
 
     }
 
-    public void arbetslivButtonClicked(){
-        System.out.println("Arbetslivserfarenhet");
+    public void educationButtonClicked()throws Exception{
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("education.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
-    }
-
-    public void utbildningarButtonClicked(){
-        System.out.println("Utbildningar");
+        stage.setScene(scene);
+        stage.setTitle("Education, Patrik J");
+        stage.show();
 
     }
 
